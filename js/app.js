08ea23544f936d7e72f945045848766fc9a52851
Nishchal Siddharth Pandey <nisiddharth@gmail.com>
2022-02-04 15:28:38 +0530

    Persistent color mode

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
+const setDarkMode = () => {
+    $('#css-mode').attr('class', 'fas fa-sun btn btn-info')
+    $('link#styles').attr('href', './css/style-gruvbox.css')
+    $('#mode-text').text('-- Go light')
+    localStorage.setItem('dark', 'on');
+}
+
+const setLightMode = () => {
+    $('#css-mode').attr('class', 'fas fa-cloud-moon btn btn-info')
+    $('link#styles').attr('href', './css/style-light.css')
+    $('#mode-text').text('-- Go dark')
+    localStorage.setItem('dark', 'off');
+}
+
+const toggleTheme = () => {
+    if ($('link#styles').attr('href') == "./css/style-light.css") { // Light mode is on
+        setDarkMode();
+    } else {  // Dark mode is on
+        setLightMode();
+    }
+}
+
 jQuery(function () {
-    $('#css-mode').on('click',function () {
-        if ($('link#styles').attr('href') == "./css/style-light.css") {
-            $('#css-mode').attr('class', 'fas fa-sun btn btn-info')
-            $('link#styles').attr('href', './css/style-gruvbox.css')
-            $('#mode-text').text('-- Go light')
-        }
-        else {
-            $('#css-mode').attr('class', 'fas fa-cloud-moon btn btn-info')
-            $('link#styles').attr('href', './css/style-light.css')
-            $('#mode-text').text('-- Go dark')
-        }
-    })
+    $('#css-mode').on('click', toggleTheme);
+
+    let getDarkMode = localStorage.getItem('dark');
+    // Check dark mode is on or off on page reload
+    if (getDarkMode === 'on') {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
 });
